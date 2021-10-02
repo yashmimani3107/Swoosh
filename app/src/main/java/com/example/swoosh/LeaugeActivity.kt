aaa9ae620d67f9489662ea1e1a88ef52606fa878
Yash Mimani <yashmimani3107@gmail.com>
2021-10-02 20:54:16 +0530

    second layout for selecting option

---
+++ app/src/main/java/com/example/swoosh/LeaugeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaugeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leauge)
+    }
+}
