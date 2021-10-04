cc650e8fb80075a6d4621c6ecc5b2f7b9465394e
Yash Mimani <yashmimani3107@gmail.com>
2021-10-04 19:50:44 +0530

    intent and content

--- app/src/main/java/com/example/swoosh/LeaugeActivity.kt
+++ app/src/main/java/com/example/swoosh/LeaugeActivity.kt
@@ -1,11 +1,18 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LeaugeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leauge)
+
+        getStratedButton.setOnClickListener {
+            val leagueIntent = Intent(this,LeaugeActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
